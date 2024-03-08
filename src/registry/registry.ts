614604833c2d981081fdb720f0bcf53e6f32c62f
louis-mtyr <94978517+louis-mtyr@users.noreply.github.com>
2024-03-08 12:57:12 +0100

    test

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -23,6 +23,14 @@ export async function launchRegistry() {
     res.send("live");
   });
 
+  // You should create an HTTP POST route called /registerNode which allows for nodes to register themselves on the registry.
+  _registry.post("/registerNode", (req: Request<{}, {}, RegisterNodeBody>, res) => {
+    const { nodeId, pubKey } = req.body;
+    const nodes = _registry.locals.nodes as Node[];
+    nodes.push({ nodeId, pubKey });
+    res.send({ result: "ok" });
+  });
+
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
   });
